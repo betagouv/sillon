542b57061bfe0fc2cd6b6dbac1c77065edd4f512
Thomas Ramé <thomas.rame@outlook.com>
2023-10-13 13:38:48 +0200

    feat: add tally form example

--- src/components/CaptionedImage.tsx
+++ src/components/CaptionedImage.tsx
@@ -1,17 +1,18 @@
 import Link from '@docusaurus/Link';
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 interface CaptionedImageProps {
   src: string;
   alt: string;
   caption?: string;
   source?: string;
+  style?: CSSProperties;
 }
 
-export function CaptionedImage({ src, alt, caption, source }: CaptionedImageProps) {
+export function CaptionedImage({ src, alt, caption, source, style }: CaptionedImageProps) {
   return (
     <figure>
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} style={style} />
       {(!!caption || !!source) && (
         <figcaption
           style={{
